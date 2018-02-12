5d0c84fd79b48a832664982ff289c6a836e5c6c3
biezhihua <biezhihua@gmail.com>
2018-02-12 11:13:41 +0800

    Android Architecture

--- app/src/main/java/com/bzh/dytt/services/ColorHuntService.java
+++ app/src/main/java/com/bzh/dytt/services/ColorHuntService.java
@@ -1,6 +1,5 @@
 package com.bzh.dytt.services;
 
-import io.reactivex.Observable;
 import okhttp3.RequestBody;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
@@ -8,31 +7,31 @@ import retrofit2.http.POST;
 
 public interface ColorHuntService {
 
-    @Headers("Content-Type: application/x-www-form-urlencoded")
-    @POST("hunt.php")
-    Observable<String> listColor(@Body RequestBody requestBod);
-
-    enum ColorHuntSort {
-        Random,
-        New,
-        Popular,
-        Hot
-    }
-
-    class ColorHuntBody {
-        public ColorHuntBody(int step, ColorHuntSort sort, String names) {
-            this.step = step;
-            this.sort = sort;
-            this.names = names;
-        }
-
-        private int step;
-        private ColorHuntSort sort;
-        private String names;
-
-        @Override
-        public String toString() {
-            return "step=" + step + "&sort=" + sort.toString().toLowerCase() + "&names=" + names;
-        }
-    }
+//    @Headers("Content-Type: application/x-www-form-urlencoded")
+//    @POST("hunt.php")
+//    Observable<String> listColor(@Body RequestBody requestBod);
+//
+//    enum ColorHuntSort {
+//        Random,
+//        New,
+//        Popular,
+//        Hot
+//    }
+//
+//    class ColorHuntBody {
+//        public ColorHuntBody(int step, ColorHuntSort sort, String names) {
+//            this.step = step;
+//            this.sort = sort;
+//            this.names = names;
+//        }
+//
+//        private int step;
+//        private ColorHuntSort sort;
+//        private String names;
+//
+//        @Override
+//        public String toString() {
+//            return "step=" + step + "&sort=" + sort.toString().toLowerCase() + "&names=" + names;
+//        }
+//    }
 }
--- app/src/main/java/com/bzh/dytt/services/ServiceManager.java
+++ app/src/main/java/com/bzh/dytt/services/ServiceManager.java
@@ -1,7 +1,7 @@
 package com.bzh.dytt.services;
 
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+//import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
@@ -19,16 +19,16 @@ public class ServiceManager {
     private ColorHuntService mColorHuntService;
 
     public ColorHuntService getColorHuntService() {
-        if (mColorHuntService == null) {
-            Retrofit retrofit = new Retrofit.Builder()
-                    .addConverterFactory(ScalarsConverterFactory.create())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .baseUrl("http://colorhunt.co/")
-                    .build();
-
-            mColorHuntService = retrofit.create(ColorHuntService.class);
-        }
+//        if (mColorHuntService == null) {
+//            Retrofit retrofit = new Retrofit.Builder()
+//                    .addConverterFactory(ScalarsConverterFactory.create())
+//                    .addConverterFactory(GsonConverterFactory.create())
+//                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+//                    .baseUrl("http://colorhunt.co/")
+//                    .build();
+//
+//            mColorHuntService = retrofit.create(ColorHuntService.class);
+//        }
         return mColorHuntService;
     }
 }
