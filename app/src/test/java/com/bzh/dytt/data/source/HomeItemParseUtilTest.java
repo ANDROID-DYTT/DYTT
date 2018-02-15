0cb3a683fe676e29a2e50c2799036bbb04cbc7bb
biezhihua <biezhihua@gmail.com>
2018-02-15 17:23:57 +0800

    Parse Area

--- app/src/test/java/com/bzh/dytt/data/source/HomeItemParseUtilTest.java
+++ app/src/test/java/com/bzh/dytt/data/source/HomeItemParseUtilTest.java
@@ -70,8 +70,19 @@ public class HomeItemParseUtilTest {
         assertEquals("/html/gndy/jddy/20180103/55966.html", result.get(result.size() - 1).getDetailLink());
     }
 
+
     @Test
-    public void parseNewest() {
+    public void parseNewest168Area() {
+        List<HomeArea> result = mNewest168Parse.parseAreas(mHomePage);
+
+        assertNotNull(result);
+        assertTrue(result.size() == 1);
+        assertEquals("最新发布168部影视", result.get(0).getTitle());
+        assertEquals("", result.get(0).getDetailLink());
+    }
+
+    @Test
+    public void parseNewestItem() {
         List<HomeItem> result = mNewestParse.parseItems(mHomePage);
 
         assertNotNull(result);
@@ -81,9 +92,19 @@ public class HomeItemParseUtilTest {
         assertEquals("2018-02-12", result.get(0).getTime());
     }
 
+    @Test
+    public void parseNewestArea() {
+        List<HomeArea> result = mNewestParse.parseAreas(mHomePage);
+
+        assertNotNull(result);
+        assertTrue(result.size() == 1);
+        assertEquals("2018新片精品", result.get(0).getTitle());
+        assertEquals("/html/gndy/dyzz/index.html", result.get(0).getDetailLink());
+    }
+
 
     @Test
-    public void parseThunder() {
+    public void parseThunderItem() {
         List<HomeItem> result = mThunderParse.parseItems(mHomePage);
 
         assertNotNull(result);
@@ -94,7 +115,18 @@ public class HomeItemParseUtilTest {
     }
 
     @Test
-    public void parseChinaTV() {
+    public void parseThunderArea() {
+        List<HomeArea> result = mThunderParse.parseAreas(mHomePage);
+
+        assertNotNull(result);
+        assertTrue(result.size() == 1);
+        assertEquals("迅雷电影资源", result.get(0).getTitle());
+        assertEquals("/html/gndy/index.html", result.get(0).getDetailLink());
+    }
+
+
+    @Test
+    public void parseChinaTVItem() {
         List<HomeItem> result = mChinaTvParse.parseItems(mHomePage);
 
         assertNotNull(result);
@@ -104,8 +136,19 @@ public class HomeItemParseUtilTest {
         assertEquals("2018-02-12", result.get(0).getTime());
     }
 
+
     @Test
-    public void parseJSKTV() {
+    public void parseChinaTVArea() {
+        List<HomeArea> result = mChinaTvParse.parseAreas(mHomePage);
+
+        assertNotNull(result);
+        assertTrue(result.size() == 1);
+        assertEquals("华语电视剧", result.get(0).getTitle());
+        assertEquals("/html/tv/hytv/", result.get(0).getDetailLink());
+    }
+
+    @Test
+    public void parseJSKTVItem() {
         List<HomeItem> result = mJSKParse.parseItems(mHomePage);
 
         assertNotNull(result);
@@ -116,7 +159,17 @@ public class HomeItemParseUtilTest {
     }
 
     @Test
-    public void parseEATV() {
+    public void parseJSKTVArea() {
+        List<HomeArea> result = mJSKParse.parseAreas(mHomePage);
+
+        assertNotNull(result);
+        assertTrue(result.size() > 0);
+        assertEquals("日韩电视剧", result.get(0).getTitle());
+        assertEquals("http://www.dytt8.net/html/tv/rihantv/index.html", result.get(0).getDetailLink());
+    }
+
+    @Test
+    public void parseEATVItem() {
         List<HomeItem> result = mEAParse.parseItems(mHomePage);
 
         assertNotNull(result);
@@ -125,5 +178,17 @@ public class HomeItemParseUtilTest {
         assertEquals("/html/tv/oumeitv/20180213/56316.html", result.get(0).getDetailLink());
         assertEquals("2018-02-13", result.get(0).getTime());
     }
+
+
+    @Test
+    public void parseEATVArea() {
+        List<HomeArea> result = mEAParse.parseAreas(mHomePage);
+
+        assertNotNull(result);
+        assertTrue(result.size() > 0);
+        assertEquals("欧美电视剧", result.get(0).getTitle());
+        assertEquals("/html/tv/oumeitv/index.html", result.get(0).getDetailLink());
+    }
+
 
 }
