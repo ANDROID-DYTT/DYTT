ebf2553a8d1aea997669ba93e67c4cf6115695b8
biezhihua <biezhihua@gmail.com>
2018-02-27 23:43:54 +0800

    [Optimize] HomePageFragment UI Test

--- app/src/androidTest/java/com/bzh/dytt/home/HomePageFragmentTest.java
+++ app/src/androidTest/java/com/bzh/dytt/home/HomePageFragmentTest.java
@@ -3,7 +3,7 @@ package com.bzh.dytt.home;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.bzh.dytt.MainActivity;
+import com.bzh.dytt.testing.SingleFragmentActivity;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,38 +12,22 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.not;
+import static org.mockito.Mockito.mock;
 
 @RunWith(AndroidJUnit4.class)
 public class HomePageFragmentTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    public ActivityTestRule<SingleFragmentActivity> mActivityTestRule = new ActivityTestRule<SingleFragmentActivity>(SingleFragmentActivity.class, true, true);
+    private HomePageFragment mHomePageFragment;
+    private HomePageViewModel mHomePageViewModel;
 
     @Before
-    public void waitForDbCreation() throws Throwable {
-//        CountDownLatch latch = new CountDownLatch(1);
-//        MutableLiveData<Boolean> databaseCreated = AppDatabase.getInstance(InstrumentationRegistry.getTargetContext()).getIsDatabaseCreated();
-//        mActivityTestRule.runOnUiThread(new Runnable() {
-//            @Override
-//            public void run() {
-//                databaseCreated.observeForever(new Observer<Boolean>() {
-//                    @Override
-//                    public void onChanged(@Nullable Boolean aBoolean) {
-//                        if (Boolean.TRUE.equals(aBoolean)) {
-//                            databaseCreated.removeObserver(this);
-//                            latch.countDown();
-//                        }
-//                    }
-//                });
-//            }
-//        });
-//
-//        MatcherAssert.assertThat("database should've initialized",
-//                latch.await(1, TimeUnit.MINUTES), CoreMatchers.is(true));
+    public void init(){
 
+        mHomePageFragment = HomePageFragment.newInstance();
+        mHomePageViewModel = mock(HomePageViewModel.class);
     }
 
     @Test
