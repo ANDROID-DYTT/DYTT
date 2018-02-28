072f7191199bb3165b1bdf8f4fa78b805fbce608
biezhihua <biezhihua@gmail.com>
2018-02-28 23:50:31 +0800

    [Feature] Fix Test Env Error & HomePage UI Test

--- app/src/androidTest/java/com/bzh/dytt/home/HomePageFragmentTest.java
+++ app/src/androidTest/java/com/bzh/dytt/home/HomePageFragmentTest.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.MutableLiveData;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.bzh.dytt.R;
 import com.bzh.dytt.data.HomeArea;
 import com.bzh.dytt.data.HomeItem;
 import com.bzh.dytt.data.HomeType;
@@ -17,8 +18,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -50,11 +61,42 @@ public class HomePageFragmentTest {
     }
 
     @Test
-    public void testTabLayoutClick() {
-
-//        onView(withText("欧美电视剧")).perform(click());
-//
-//        onView(withText("2018新片精品")).perform(click());
+    public void success(){
+        HomeArea homeArea = new HomeArea("别志华", HomeType.NEWEST_168);
+        HomeArea homeArea2 = new HomeArea("胡玉琼", HomeType.NEWEST);
+        List<HomeArea> list = new ArrayList<>();
+        list.add(homeArea);
+        list.add(homeArea2);
+        mHomeAreas.postValue(Resource.success(list));
+        onView(withId(R.id.home_swipe_refresh)).check(matches(not(isEnabled())));
+        onView(withId(R.id.home_error)).check(matches(not(isDisplayed())));
+        onView(withText(homeArea.getTitle())).check(matches(isDisplayed()));
+        onView(withText(homeArea2.getTitle())).check(matches(isDisplayed()));
+        onView(withText(homeArea.getTitle())).perform(click());
+        onView(withText(homeArea2.getTitle())).perform(click());
     }
 
+    @Test
+    public void loading() {
+        mHomeAreas.postValue(Resource.loading(null));
+        onView(withId(R.id.home_swipe_refresh)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void errorNullData() {
+        mHomeAreas.postValue(Resource.error("wtf", null));
+        onView(withId(R.id.home_swipe_refresh)).check(matches(isEnabled()));
+        onView(withId(R.id.home_error)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void errorHaveData() {
+        List<HomeArea> list = new ArrayList<>();
+        HomeArea homeArea = new HomeArea("别志华", HomeType.NEWEST_168);
+        list.add(homeArea);
+        mHomeAreas.postValue(Resource.error("wtf", list));
+        onView(withId(R.id.home_swipe_refresh)).check(matches(not(isEnabled())));
+        onView(withId(R.id.home_error)).check(matches(not(isDisplayed())));
+        onView(withText(homeArea.getTitle())).check(matches(isDisplayed()));
+    }
 }
